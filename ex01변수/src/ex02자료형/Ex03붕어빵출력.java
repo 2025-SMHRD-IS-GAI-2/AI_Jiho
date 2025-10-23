7918981a5da291443004a05f2339a9c808244b12
smhrd <smhrd@192.168.219.56>
2025-10-23 10:22:36 +0900

    첫 커밋 완료! -> 특정 시점의 스냅샷

---
+++ ex01변수/src/ex02자료형/Ex03붕어빵출력.java
@@ -0,0 +1,14 @@
+package ex02자료형;
+
+import java.util.Scanner;
+
+public class Ex03붕어빵출력 {
+
+	public static void main(String[] args) {
+	System.out.println("구매 희망하는 팥붕 개수 : ");
+	Scanner sc = new Scanner(System.in);
+	int num=sc.nextInt();
+	System.out.println("붕어빵의 개수 : "+num);
+	}
+
+}
---
+++ ex01변수/src/ex02자료형/Ex복습.java
@@ -0,0 +1,5 @@
+package ex02자료형;
+
+public class Ex복습 {
+
+}
